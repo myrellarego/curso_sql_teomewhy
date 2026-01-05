9bba87484ced62db5340791910071c3d81608b2e
myrellarego <66757480+myrellarego@users.noreply.github.com>
2026-01-05 10:47:44 -0300

    Enhance query to include max and min attendance
    Added calculations for max and min attendance days.

--- curso-sql/bora_praticar/bloco4 - join/11_exercicio - join - cte.sql
+++ curso-sql/bora_praticar/bloco4 - join/11_exercicio - join - cte.sql
@@ -2,6 +2,7 @@
 
 -- selecionar todos os clientes que começaram no primeiro dia. os clientes precisam ser distintos
 
+-- 1) quem participou da primeira aula?
 WITH tb_prim_dia AS (
 
     SELECT DISTINCT IdCliente AS IdCliente1dia
@@ -11,7 +12,7 @@ WITH tb_prim_dia AS (
 ),
 
 -- descobrir a quantidade de transacoes durante os dias de curso. para isso pego o cliente e definindo os dias que ele esteve presente. essa consulta dá uma visao do inicio ao fim do curso
-
+-- 2) quem participou do curso inteiro?
 tb_dias_curso AS (
 
     SELECT  DISTINCT
@@ -25,7 +26,7 @@ tb_dias_curso AS (
 ),
 
 -- agora vou pegar apenas as pessoas que estiveram no 1º dia e eu busco quais outros dias ela apareceu e por ultimo agrupo contando quantos dias cada cliente esteve
-
+-- 3) contando quantas vezes quem participou do primeiro dia voltou
 tb_cliente_dias AS (
 
 SELECT t1.IdCliente1dia,
@@ -41,6 +42,9 @@ GROUP BY t1.IdCliente1dia
 )
 
 -- agora, por fim, posso descobrir a média de dias
+-- 3) calculo da média 
 
-SELECT avg(qtdDeDiasPresente)
+SELECT avg(qtdDeDiasPresente),
+       max(qtdDeDiasPresente),
+       min(qtdDeDiasPresente)
 FROM tb_cliente_dias
